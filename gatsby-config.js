775c6364e2b2f4caf64a401df7180e9495a92f1f
alexHoggett <alexmh99@gmail.com>
2022-12-21 14:30:32 +0000

    learning about some plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,8 @@ module.exports = {
     title: `Portfolio`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 }
